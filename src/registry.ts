e523682a6227c332d984db0eb125ae8f12aa314a
Jeudi Prando <jeudiprando@gmail.com>
2024-03-12 07:36:27 -0300

    refactor(registry): 🚀 utilizar env var REGISTRY para se conectar ao schema registry

--- src/registry.ts
+++ src/registry.ts
@@ -1,5 +1,9 @@
+import assert from "node:assert";
 import { SchemaRegistry } from "@kafkajs/confluent-schema-registry";
 
+const { REGISTRY } = process.env;
+assert(REGISTRY, "REGISTRY nao esta configurado");
+
 export default new SchemaRegistry({
-  host: "http://broker.prando.dev.br:18081/",
+  host: REGISTRY,
 });
