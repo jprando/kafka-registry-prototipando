8369345f11cf8e61daf9889cf1d1b3ea4c4febe1
Jeudi Prando <jeudiprando@gmail.com>
2024-03-12 09:40:56 -0300

    style: 🎨 formatacao de codigo

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,16 @@ const ehParaExecutar = process.argv.at(-1);
 console.info({ ehParaExecutar });
 
 switch (ehParaExecutar) {
-  case "enviarMensagem":
+  case "enviarMensagem": {
     const producers = executarEnviarMensagem(broker);
     console.info(producers.length, "producers criados...");
     break;
-  case "receberMensagem":
+  }
+  case "receberMensagem": {
     const consumers = executarReceberMensagem(broker);
     console.info(consumers.length, "consumers criados...");
     break;
+  }
   default:
     console.error("opcao nao conhecida, saindo...");
 }
